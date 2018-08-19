21dc8528d1d3027fcf86071c260d87d347c08af9
An Cao <kcj.pop.com@gmail.com>
2018-08-19 14:11:26 +0300

    Scroll to top when changing pages
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 import Layout from '@/modules/ui/Layout'
+import ScrollToTop from '@/modules/ui/ScrollToTop'
 import Sidebar from '@/modules/articles/components/List'
 
 import { store, routes } from './bootstrap'
@@ -11,14 +12,16 @@ class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <Layout sidebar={<Sidebar />}>
-          <Switch>
-            {routes.map(({ server, ...route }) => (
-              // Extract server configuration out
-              <Route key={route.path} {...route} />
-            ))}
-          </Switch>
-        </Layout>
+        <ScrollToTop>
+          <Layout sidebar={<Sidebar />}>
+            <Switch>
+              {routes.map(({ server, ...route }) => (
+                // Extract server configuration out
+                <Route key={route.path} {...route} />
+              ))}
+            </Switch>
+          </Layout>
+        </ScrollToTop>
       </Provider>
     )
   }
