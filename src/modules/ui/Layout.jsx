4800b13c7ab18b4059bef49df4f2daae365a5012
An Cao <kcj.pop.com@gmail.com>
2018-08-29 17:02:23 +0300

    Change horizontal padding on mobile
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- src/modules/ui/Layout.jsx
+++ src/modules/ui/Layout.jsx
@@ -5,7 +5,7 @@ function Layout(props) {
   return (
     <div className="flex flex-column-reverse flex-row-l serif f4">
       <div className="w-25-l bg-dark-gray pa3">{props.sidebar}</div>
-      <div className="w-75-l ph5 pv3">{props.children}</div>
+      <div className="w-75-l ph3 ph5-ns pv3">{props.children}</div>
     </div>
   )
 }
